a04cffcf210ee69e5c9c8db0fcffa6c418a126e1
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-31 13:54:05 +0200

    init react setup v18

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { React, useState, useEffect } from "react";
+import http from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>todo</h1>
+    </div>
+  );
+}
+
+export default App;
