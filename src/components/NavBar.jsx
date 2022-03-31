6b91ba6b6d9f7886969526a33c6048da64ae7278
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-03-31 16:36:25 +0200

    header with style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/shared/logo.svg"
 
 const NavBar = () => {
   return (
     <div className="primary-header flex">
-      <div></div>
+      <div>
+        <img src={logo} alt="space tourism" />
+      </div>
       <nav>
         <ul className="primary-navigation underline-indicators flex">
-          <Link to="/">
-            <li className="active"><p className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2"><span>00</span>Home</p></li>
-          </Link>
-          <Link to="/destination">
-            <li><p className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2"><span>01</span>Destination</p></li>
-          </Link>
-          <Link to="/crew">
-            <li><p className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2"><span>02</span>Crew</p></li>
-          </Link>
-          <Link to="/technology">
-            <li><p className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2"><span>03</span>Technology</p></li>
-          </Link>
+            <NavLink to="/">
+              <li className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2">
+              <span>00</span>Home</li>
+            </NavLink>
+            <NavLink to="/destination">
+              <li className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2">
+              <span>01</span>Destinations</li>
+            </NavLink>
+            <NavLink to="/crew">
+              <li className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2">
+              <span>02</span>Crew</li>
+            </NavLink>
+            <NavLink to="/technology">
+              <li className="uppercase letter-spacing-2 text-white ff-sans-cond fs-300 letter-spacing-2">
+              <span>03</span>Technology</li>
+            </NavLink>
+
+
         </ul>
       </nav>
     </div>
