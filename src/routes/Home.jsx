96e4d8af663f1236e3fe6e9681e215499a7b8443
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-04-01 14:07:47 +0200

    rework home grid layout...

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="grid-container">
+    <div className="grid-container grid-container--home">
       <div>
         <h1 className="ff-sans-cond fs-500 uppercase text-accent letter-spacing-1">So, you want to travel to
-          <span className="fs-900 ff-serif text-white">Space</span>
+          <span className="fs-900 ff-serif text-white d-block">Space</span>
         </h1>
         <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
       </div>
       <div>
-        <a href="/" className="large-button bg-white ff-serif text-dark fs-600 uppercase" style={{letterSpacing: "2px"}}>Explore</a>
+        <a href="/" className="large-button bg-white ff-serif text-dark uppercase letter-spacing-4">Explore</a>
       </div>
     </div>
   )
 }
 
 export default Home
+
+/* homepage part 3 */
